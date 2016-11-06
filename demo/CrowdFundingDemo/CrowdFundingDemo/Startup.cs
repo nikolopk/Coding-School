25ee173b16a5b171b5b9eb5651b32091bbce2df5
chpapantonis <chpapantonis@phys.uoa.gr>
2016-11-06 23:12:02 +0200

    Added a demo MVC project. Nothing has been touched. Just initialised it so we all have something to play with. :)

---
+++ demo/CrowdFundingDemo/CrowdFundingDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrowdFundingDemo.Startup))]
+namespace CrowdFundingDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
