47eb4da85f341ebb1d4696727bfae4b551d56a05
tania2 <taniaaa85@gmail.com>
2016-12-03 11:15:59 +0200

    Edit project

---
+++ CrowdFundingV2/WebApplication1/WebApplication1/Models/RewardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class RewardViewModel
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int MinAmount { get; set; }
+        public int MaxAmount { get; set; }
+        public int MaxAvailable { get; set; }
+        public int CurrentAvailable { get; set; }
+    }
+}
