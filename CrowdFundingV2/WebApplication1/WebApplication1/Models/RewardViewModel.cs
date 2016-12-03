60c12236614dd305b7cb6a8fee52077b6d34bba2
tania2 <taniaaa85@gmail.com>
2016-12-03 13:48:37 +0200

    Create rewards

--- CrowdFundingV2/WebApplication1/WebApplication1/Models/RewardViewModel.cs
+++ CrowdFundingV2/WebApplication1/WebApplication1/Models/RewardViewModel.cs
@@ -6,15 +6,20 @@ namespace WebApplication1.Models
     {
         public int Id { get; set; }
         public int ProjectId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
 
-        [Display(Name = "Required Amount")]
-        public decimal RequiredAmount { get; set; } 
-
+        [Required]
+        [Display(Name = "Minimum Required Amount")]
         public int MinAmount { get; set; }
+
+        [Required]
+        [Display(Name = "Maximum Required Amount")]
         public int MaxAmount { get; set; }
         [Display(Name = "Max Available")]
+        [Required]
         public int MaxAvailable { get; set; }
         public int CurrentAvailable { get; set; }
     }
