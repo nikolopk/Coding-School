8eaa12de610ecbf11ad388c33eda4c74b47ad64f
chpapantonis <chpapantonis@phys.uoa.gr>
2016-12-03 12:18:12 +0200

    added backProject feature with textbox

--- CrowdFundingV2/WebApplication1/WebApplication1/Models/CategoryViewModel.cs
+++ CrowdFundingV2/WebApplication1/WebApplication1/Models/CategoryViewModel.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace WebApplication1.Models
-{
+﻿namespace WebApplication1.Models {
     public class CategoryViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
         public int NoProjects { get; set; }
     }
 }
---
+++ CrowdFundingV2/WebApplication1/WebApplication1/Models/TransactionViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication1.Models {
+    public class TransactionViewModel {
+        public bool transaction { get; set; }
+        public int projectId { get; set; }
+    }
+}
